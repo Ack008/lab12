d389cbc4f52a312a545cfe0d9f2974360ab2d99e
Francesco Sacripante - francesco.sacripante@studio.unibo.it <francesco.sacripante@studio.unibo.it>
2024-12-10 15:21:42 +0100

    Applying correction to exercise 1

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -4,32 +4,30 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LogicsImpl implements Logics {
 
-	private final int size;
-	private final List<Integer> buttonsValues; 
+	private final List<Integer> buttonsValues;
+
 	public LogicsImpl(int size) {
-		this.size = size;
-		this.buttonsValues = Stream.of(0,0,0,0).collect(Collectors.toList());
+		this.buttonsValues = Stream.generate(() -> 0).limit(size).toList();
 	}
 
 	@Override
 	public int size() {
-		return size;
+		return buttonsValues.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		return buttonsValues.subList(0, size);
+		return Collections.unmodifiableList(buttonsValues);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		return buttonsValues.stream().map(i -> i < size).toList();
+		return buttonsValues.stream().map(i -> i < buttonsValues.size()).toList();
 	}
 
 	@Override
@@ -41,26 +39,25 @@ public class LogicsImpl implements Logics {
 
 	@Override
 	public String result() {
-		String result = "<<";
-		for(int i = 0; i < size; i++ ){
-			result = result + buttonsValues.get(i);
-			if(i != size - 1) {
-				result = result + "|";
-			}
-		}
-		result = result + ">>";
-		return result;
+		return buttonsValues.stream()
+			.mapToInt(i -> i)
+			.mapToObj(Integer::toString)
+			.collect(Collectors.joining("|", "<<", ">>"));
+		// String result = "<<";
+		// for(int i = 0; i < size; i++ ){
+		// 	result = result + buttonsValues.get(i);
+		// 	if(i != size - 1) {
+		// 		result = result + "|";
+		// 	}
+		// }
+		// result = result + ">>";
+		// return result;
 	}
 
 	@Override
 	public boolean toQuit() {
-		boolean quit = true;
-		int value = buttonsValues.getFirst();
-		for(final int i : buttonsValues){
-			if(value != i) {
-				quit = false;
-			}
-		}
-		return quit;
+		final var first = buttonsValues.getFirst();
+		return first > 0 &&
+			buttonsValues.stream().skip(1).allMatch(it -> it == first);
 	}
 }
