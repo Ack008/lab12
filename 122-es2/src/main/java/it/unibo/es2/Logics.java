5ab992f80b61acd79035b286401774fbee54a200
Francesco Sacripante - francesco.sacripante@studio.unibo.it <francesco.sacripante@studio.unibo.it>
2024-12-10 12:05:06 +0100

    Adding the logics interface

---
+++ 122-es2/src/main/java/it/unibo/es2/Logics.java
@@ -0,0 +1,19 @@
+package it.unibo.es2;
+
+public interface Logics {
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return
+     */
+    public String getText(final int x ,final int y);
+
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return
+     */
+    public boolean quit(final int x, final int y);
+}
